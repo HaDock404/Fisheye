eab837e9ca1c3ac77bdb7c5b9bd825fe1bcce6c4
HaDock404 <delescluse.g@gmail.com>
2022-05-10 13:52:51 +0200

    Préparation soutenance

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,42 +1,18 @@
-/*fetch("data/photographers.json")
-    .then(res=>res.json())
-    .then (function(data) {
-        let i = 0;
-
-        while (i<data.photographers.length) {
-
-            const profilPicture = data.photographers[i].portrait;
-            const photographerName = data.photographers[i].name;
-            const locationCity = data.photographers[i].city;
-            const locationCountry = data.photographers[i].country;
-            const tagLine = data.photographers[i].tagline;
-            const price = data.photographers[i].price;
-            const id = data.photographers[i].id;
-
-
-            const article = document.getElementById("test");
-
-            article.innerHTML += "<div class='profil'><a href='photographer.html?id="+id+"'><img class='image' src='assets/photographers/"+profilPicture+"' alt=''></a><h2>"+photographerName+"</h2><h3>"+locationCity+", "+locationCountry+"</h3><h4>"+tagLine+"</h4><div class='price'>"+price+"€/jour</div></div>"
-
-            i++;
-        }
-    })*/
-
-
-
+/*********************Create a Class Object*************************/
     class Api {
         constructor(url) {
             this._url = url
         }
     
         async get() {
-            return fetch(this._url)
+            return fetch(this._url)/*Fetch take data from a JSON file*/
                 .then(res => res.json())
                 .then(res => res.photographers)
                 .catch(err => console.log('an error occurs', err))
         }
     }
     
+/************Create a Class legacy with "extends" & "super"****************/
     class PhotographerApi extends Api {
         
         constructor(url) {
@@ -48,23 +24,22 @@
         }
     }
     
-    
-    
-    
+/* Create a class with a function, this function add dynamic HTML with data JSON*/
     class PhotographerCard {
         constructor(profil) {
             this._profil = profil
         }
     
         createPhotographerCard() {
-            const article = document.getElementById("test");
+            const article = document.getElementById("card");
     
-            const photographerCard = "<div class='profil'><a href='photographer.html?id="+this._profil.id+"'><img class='image' src='assets/photographers/"+this._profil.portrait+"' alt=''></a><h2>"+this._profil.name+"</h2><h3>"+this._profil.city+", "+this._profil.country+"</h3><h4>"+this._profil.tagline+"</h4><div class='price'>"+this._profil.price+"€/jour</div></div>"
+            const photographerCard = "<a class='profil' href='photographer.html?id="+this._profil.id+"'><img class='profil-picture' src='assets/photographers/"+this._profil.portrait+"' alt='"+this._profil.name+"' aria-label='Photo de profil'><h2 aria-label='Nom du photographe'>"+this._profil.name+"</h2><h3 aria-label='Localisation du photographe'>"+this._profil.city+", "+this._profil.country+"</h3><h4 aria-label='Mantra du photographe'>"+this._profil.tagline+"</h4><div class='price' aria-label='Prix du photographe à la journée'>"+this._profil.price+"€/jour</div></a>"
             
             article.innerHTML += photographerCard
         }
     }
       
+/**************Class Object give file for the Class Legacy**************/
     class App {
         constructor() {
             this.photographerApi = new PhotographerApi('data/photographers.json')
@@ -73,12 +48,14 @@
         async main() {
             const cards = await this.photographerApi.getPhotographer()
     
-            cards.forEach(profil => {
+            cards.forEach(profil => {/**For each Object use the previous function*/
                 const Template = new PhotographerCard(profil)
                 Template.createPhotographerCard()       
             })    
         }
     }
     
+
+/**Use the Class Object everytime user load the page*/    
     const app = new App();
     app.main();
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,36 +1,40 @@
+/**Get id of photographer in the URL */
     let params = (new URL(document.location)).searchParams;
     const idPhotographer = params.get('id');
+/*************************************/
+    
+let totalLikes = 0;/**Init total like */
 
-    let totalLikes = 0;
-
-
+/**Table of Data from JSON */
     class Card {
-        constructor(picture, title, like, date, photographerId, id) {
+        constructor(picture, title, like, date, photographerId, id, video) {
             this.picture = picture;
             this.title = title;
             this.like = like;
             this.date = date;
             this.photographerId = photographerId;
             this.id = id;
+            this.video = video;
         }
     }
+/***************************/
 
-    let tabCard = [];
+    let tabCard = [];/**Storage for table Card */
 
-
+/*********************Create a Class Object*************************/
     class Api {
         constructor(url) {
             this._url = url
         }
     
-        async getPhotographerData() {
+        async getPhotographerData() {/**Get data from photographers Object */
             return fetch(this._url)
                 .then(res => res.json())
                 .then(res => res.photographers)
                 .catch(err => console.log('an error occurs', err))
         }
 
-        async getPictureData() {
+        async getPictureData() {/**Get data from media Object */
             return fetch(this._url)
                 .then(res => res.json())
                 .then(res => res.media)
@@ -38,24 +42,23 @@
         }
     }
     
+/************Create a Class legacy with "extends" & "super"****************/
     class PhotographerApi extends Api {
         
         constructor(url) {
             super(url)
         }
     
-        async getPhotographer() {
+        async getPhotographer() {/**Return Data from photograhers Object */
             return await this.getPhotographerData()
         }
 
-        async getPicture() {
+        async getPicture() {/**Return Data from photograhers Object */
             return await this.getPictureData()
         }
     }
     
-    
-    
-    
+/* Create a class with a function, this function add dynamic HTML with data JSON*/   
     class PhotographerCard {
         constructor(profil) {
             this._profil = profil
@@ -65,16 +68,20 @@
             const bio = document.querySelector('.photograph-bio');
             const picture = document.querySelector('.photograph-header');
             const footer = document.querySelector('.price-day');
+            const header = document.querySelector('#headerModal')
 
-            if (this._profil.id == idPhotographer) {
+            if (this._profil.id == idPhotographer) {/**Check URL matches with Data*/
                 
-                const photographerCard = "<h1 class='photographName'>"+this._profil.name+"</h1><h2 class='location'>"+this._profil.city+", "+this._profil.country+"</h2><h3 class='tagLine'>"+this._profil.tagline+"</h3>"
+                const photographerCard = "<h1 class='photographName' aria-label='Nom du photographe'>"+this._profil.name+"</h1><h2 class='location' aria-label='Localisation du photographe'>"+this._profil.city+", "+this._profil.country+"</h2><h3 class='tagLine' aria-label='Mantra du photographe'>"+this._profil.tagline+"</h3>"
 
-                const photographerPicture = '<img src="assets/photographers/'+this._profil.portrait+'" class="image" />'
+                const photographerPicture = '<img src="assets/photographers/'+this._profil.portrait+'" class="profil-picture" alt="'+this._profil.name+'" aria-label="Photo de profil du photographe"/>'
+
+                const photographerForm = '<h2>'+this._profil.name+'</h2>'
 
 
                 bio.innerHTML += photographerCard;
                 picture.innerHTML += photographerPicture;
+                header.innerHTML += photographerForm;
                 
                 footer.innerHTML += '<div class="footer">'+this._profil.price+'€ / jour</div>'
 
@@ -82,7 +89,7 @@
         }
     }
       
-
+/* Create a class with a function, this function add dynamic HTML with data JSON*/
     class PictureCard {
         constructor(profil) {
             this._profil = profil
@@ -94,22 +101,40 @@
 
             if (this._profil.photographerId == idPhotographer) {
 
-                totalLikes = totalLikes + this._profil.likes;
+                totalLikes = totalLikes + this._profil.likes;/**Increment total likes */
+
+                if (this._profil.image == undefined) {/**Check if it's not a picture -> video */
+                    
+                    const pictureCard = "<article class='publication' aria-label='Post du photographe'><video id='"+this._profil.id+"' class='postPicture' alt='"+this._profil.title+"' aria-label='Vidéo'><source src='assets/images/"+this._profil.video+"' type='video/mp4'></video><div class='stats'><h2 class='pictureTitle'>"+this._profil.title+" aria-label='Titre de la vidéo'</h2><h3 value='0' class='likeNumber' id='"+this._profil.id+"' aria-label='Nombre de like'>"+this._profil.likes+" <img class='heart' src='assets/icons/heart-red.svg' alt=''></img></h3></div></article>";
+
+                    post.innerHTML += pictureCard;
+
+                    /**Table for slideshow Modal*/
+                    const CardClass = new Card(this._profil.image, this._profil.title, this._profil.likes, this._profil.date, this._profil.photographerId, this._profil.id, this._profil.video);
+
+                    tabCard.push(CardClass);
                 
-                const pictureCard = "<div class='publication'><img src='assets/images/"+this._profil.image+"' class='postPicture' id='"+this._profil.id+"'/><div class='stats'><div class='pictureTitle'>"+this._profil.title+"</div><div class='likeNumber'>"+this._profil.likes+"</div></div></div>";
+                } else { /**It's a picture */
+                    
+                    const pictureCard = "<article class='publication' aria-label='Post du photographe'><img src='assets/images/"+this._profil.image+"' class='postPicture' id='"+this._profil.id+"' alt='"+this._profil.title+"' aria-label='Photo'/><div class='stats'><h2 class='pictureTitle' aria-label='Titre de la photo'>"+this._profil.title+"</h2><h3 value='0' class='likeNumber' id='"+this._profil.id+"' aria-label='Nombre de like'>"+this._profil.likes+" <img class='heart' src='assets/icons/heart-red.svg' alt=''></img></h3></div></article>";
+                    
+                    post.innerHTML += pictureCard;
 
-                post.innerHTML += pictureCard;
+                    /**Table for slideshow Modal*/
+                    const CardClass = new Card(this._profil.image, this._profil.title, this._profil.likes, this._profil.date, this._profil.photographerId, this._profil.id);
 
-                const CardClass = new Card(this._profil.image, this._profil.title, this._profil.likes, this._profil.date, this._profil.photographerId, this._profil.id);
-
-                tabCard.push(CardClass);
+                    tabCard.push(CardClass);
+                }
+                
+                
 
             }
-            footer.innerHTML = '<div class="footer">'+totalLikes+'</div>'
+            footer.innerHTML = '<div class="footer">'+totalLikes+' <img class="heartFooter" src="assets/icons/heart-solid.svg" alt=""></div>'
 
         }
     }
 
+/* Create a class with a function, this function add dynamic HTML with data JSON*/
     class PictureCardSelected {
         constructor(profil) {
             this._profil = profil
@@ -117,29 +142,23 @@
     
         createPictureCardSelected() {
             const post = document.querySelector('.post');
-    
+            
+            /**Delete all previous post */
             while (post.firstChild) {
                 post.removeChild(post.firstChild);
               }
             
               if (this._profil.photographerId == idPhotographer) {
 
-                const CardClass = new Card(this._profil.image, this._profil.title, this._profil.likes, this._profil.date, this._profil.photographerId, this._profil.id);
+                /**Table for sorting Data*/
+                const CardClass = new Card(this._profil.image, this._profil.title, this._profil.likes, this._profil.date, this._profil.photographerId, this._profil.id, this._profil.video);
 
                 tabCard.push(CardClass);
             }
         }
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    } 
+   
+/**************Class Object give file for the Class Legacy**************/
     class App {
         constructor() {
             this.photographerApi = new PhotographerApi('data/photographers.json')
@@ -161,7 +180,8 @@
                 const Template = new PictureCard(profil)
                 Template.createPictureCard()       
             })
-            eventListener();   
+            imagetListener();
+            likeListener();   
         }
 
         async like() {
@@ -202,11 +222,7 @@
     app.profil();
     app.image();
 
-    
-
-
-
-
+    /**Sorting by like */
     function bestPicture() {
 
         const changeSelect = document.querySelector('.select')
@@ -220,7 +236,7 @@
         changeSelect.innerHTML = '<div>Popularité</div>'
     }
 
-
+    /**Sorting by title */
     function titlePicture() {
 
         const changeSelect = document.querySelector('.select')
@@ -239,7 +255,7 @@
         changeSelect.innerHTML = '<div>Titre</div>'
     }
 
-
+    /**Sorting by date */
     function datePicture() {
 
         const changeSelect = document.querySelector('.select')
@@ -252,43 +268,47 @@
         changeSelect.innerHTML = '<div>Date</div>'
     }
 
-    
+   /**Display picture after sorting */ 
     function loopInnerHTML() {
         let j = 0;
         const post = document.querySelector('.post');
 
         while (j < tabCard.length) {
         
+            /**Get Data from Table Object */
             const picture = tabCard[j].picture;
             const title = tabCard[j].title;
             const likes = tabCard[j].like;
             const id = tabCard[j].id;
+            const video = tabCard[j].video;
 
-            const pictureCard = "<div class='publication'><img src='assets/images/"+picture+"' class='postPicture' id='"+id+"'/><div class='stats'><div class='pictureTitle'>"+title+"</div><div class='likeNumber'>"+likes+"</div></div></div>";
+            if (picture == undefined) {
+                
+                const pictureCard = "<article class='publication'><video  class='postPicture' id='"+id+"' alt='"+title+"' aria-label='Vidéo'><source src='assets/images/"+video+"' type='video/mp4'></video><div class='stats'><h2 class='pictureTitle' aria-label='Titre de la photo'>"+title+"</h2><h3 value='0' class='likeNumber' id='"+id+"' aria-label='Nombre de like'>"+likes+" <img class='heart' src='assets/icons/heart-red.svg' alt=''></img></h3></div></article>";
     
-            post.innerHTML += pictureCard;
+                post.innerHTML += pictureCard;
+            } else {
+                
+                const pictureCard = "<article class='publication'><img src='assets/images/"+picture+"' class='postPicture' id='"+id+"' alt='"+title+"' aria-label='Photo'/><div class='stats'><h2 class='pictureTitle' aria-label='Titre de la photo'>"+title+"</h2><h3 value='0' class='likeNumber' id='"+id+"' aria-label='Nombre de like'>"+likes+" <img class='heart' src='assets/icons/heart-red.svg' alt=''></img></h3></div></article>";
     
+                post.innerHTML += pictureCard;
+            }
             j++;
         }
-        eventListener();
-        //tabCard = [];
+        imagetListener();
+        likeListener();
     }
 
-
-
-
-
-
-
-
     const picture = document.querySelector(".picture");
+    
     const modal = document.querySelector(".modalDisplayPicture");
     const left = document.querySelector("#left");
     const right = document.querySelector("#right");
     const html = document.querySelector('html')
-    let tabModal = [];
+    let tabModal = [];/**Storage for table modale for the slideshow */
 
-    function eventListener() {
+    /**Add listener on every picture for the slideshow */
+    function imagetListener() {
 
         let image = document.getElementsByClassName("postPicture");
 
@@ -296,7 +316,50 @@
             image[i].addEventListener("click", displayPicture);
         }
     }
+ 
+    /**Add listener on every like for the increment*/
+    function likeListener() {
 
+        let like = document.getElementsByClassName("likeNumber");
+
+        for(let i = 0; i < like.length; i++) {
+            like[i].addEventListener("click", incrementLike);
+        }
+    }
+    
+    /**Increment like*/
+    function incrementLike(e) {
+
+        const attribute = e.target.getAttribute("id");
+        const footer = document.querySelector('.likes-total');
+        let attributeValue = e.target.getAttribute("value")
+        let j = 0;
+        let valueStorage = attributeValue;
+
+        
+        while (j < tabCard.length) {
+            const likeX = tabCard[j].like;
+            const idX = tabCard[j].id;
+
+            if(idX == attribute) {
+                const like = document.querySelector("h3[class=likeNumber][id="+ CSS.escape(attribute) +"")/**get variable in the attribute*/
+
+                let i = 0;
+                i += likeX;
+                ++valueStorage;
+                let likeIncrement = i + valueStorage;                
+
+                like.innerHTML = "<h3 value='"+valueStorage+"' class='likeNumber2' id='"+attribute+"'>"+likeIncrement+" <img class='heartRed' src='assets/icons/heart-solid-red.svg' alt=''></img></h3>";
+
+                ++totalLikes;
+                
+                footer.innerHTML = '<div class="footer">'+totalLikes+' <img class="heartFooter" src="assets/icons/heart-solid.svg" alt=""></div>'
+            }
+            j++;
+        }
+    }
+
+    /**Display slideshow Modal */
     function displayPicture(e) {
         
         const attribute = e.target.getAttribute("id");
@@ -310,11 +373,32 @@
             const idX = tabCard[j].id;
 
             if(idX == attribute) {
-                console.log('ok')
-                picture.innerHTML = `<img class="modalPicture" src="assets/images/`+imgX+`" alt="">`;
+
+                if(imgX == undefined) {
+
+                    const imgX = tabCard[j].video;
+
+                    picture.innerHTML = "<video controls  class='modalPicture'><source src='assets/images/"+imgX+"' type='video/mp4'></video>";
+                } else {
+
+                    picture.innerHTML = `<img class="modalPicture" src="assets/images/`+imgX+`" alt="">`;
+                }
+                
                 tabModal.push(imgX)
+                
                 left.addEventListener("click", changeLeft);
                 right.addEventListener("click", changeRight);
+
+                document.onkeydown = function(e) {
+                    switch (e.keyCode) {
+                        case 37:
+                            changeLeft();
+                            break;
+                        case 39:
+                            changeRight();
+                            break;
+                    }
+                };
             }
             j++;
         }
@@ -342,18 +426,33 @@
     function changeLeft() {
 
         let x = 0;
-        let y = 1
+        let y = 1;
 
         while (x < tabCard.length) {
             let imgY = tabCard[x].picture;
 
             if (imgY == tabModal[0]) {
                 y = x - y;
+                
                 imgY = tabCard[y].picture;
-                picture.innerHTML = `<img class="modalPicture" src="assets/images/`+imgY+`" alt="">`;
+
+                if (imgY == undefined) {
+                    imgY = tabCard[y].video;
+
+                    picture.innerHTML = "<video controls  class='modalPicture'><source src='assets/images/"+imgY+"' type='video/mp4'></video>";
+                    imgY = tabCard[y].picture;
+                } else {
+
+                    picture.innerHTML = `<img class="modalPicture" src="assets/images/`+imgY+`" alt="">`;
+                }
+                
+                
+                
                 tabModal = [];
                 tabModal.push(imgY)
+                
                 y = 1;
+                
                 left.addEventListener("click", changeLeft);
                 right.addEventListener("click", changeRight);
                 return;                
@@ -372,15 +471,32 @@
 
             if (imgY == tabModal[0]) {
                 y = y + x;
+                
                 imgY = tabCard[y].picture;
-                picture.innerHTML = `<img class="modalPicture" src="assets/images/`+imgY+`" alt="">`;
+                
+                if (imgY == undefined) {
+                    imgY = tabCard[y].video;
+
+                    picture.innerHTML = "<video controls  class='modalPicture'><source src='assets/images/"+imgY+"' type='video/mp4'></video>";
+                    imgY = tabCard[y].picture;
+                } else {
+
+                    picture.innerHTML = `<img class="modalPicture" src="assets/images/`+imgY+`" alt="">`;
+                }
+                
                 tabModal = [];
                 tabModal.push(imgY)
                 y = 1;
+                
                 left.addEventListener("click", changeLeft);
                 right.addEventListener("click", changeRight);
                 return;
             }
             x++;
         }
+    }
+
+    function testSubmitModal() {
+        closeModal();
+        return false; 
     }
